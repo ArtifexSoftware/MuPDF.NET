9471936e81be33dabd56bf299625a7a67c04600c
greendreamer <chrispalman0317@gmail.com>
2024-06-11 18:08:42 -0400

    GREEN: Added extracting stuff

---
+++ Examples/ExportEmbed/CSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MuPDF.NET;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Document doc = new Document(args[0]);
+        byte[] content = doc.GetEmbfile(0);
+        File.WriteAllBytes("../../../../output.jpg", content);
+    }
+}
---
+++ Examples/ExportEmbed/FSharp/Program.fs
@@ -0,0 +1,11 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+open MuPDF.NET
+open System
+open System.IO
+
+[<EntryPoint>]
+let main(args) =
+    let doc = new Document(args.[0])
+    let embed = doc.GetEmbfile(0)
+    File.WriteAllBytes("../../../../output.jpg", embed)
+    0
