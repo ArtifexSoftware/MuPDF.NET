a441ec7f439accb83cc482b6a92a5112216a9b36
greendreamer <chrispalman0317@gmail.com>
2024-06-11 03:55:50 -0400

    GREEN: Added C# Drawing stuff in example

---
+++ Examples/CopyEmbedded/CopyEmbedded/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MuPDF.NET;
+using System.Text;
+
+Document src = new Document("input.pdf");
+Document dst = new Document("output.pdf");
+
+for (int i = 0; i < src.GetEmbfileCount(); i++)
+{
+    EmbfileInfo d = src.GetEmbfileInfo(i);
+    byte[] b = src.GetEmbfile(i);
+    Console.WriteLine(Encoding.UTF8.GetString(b));
+    dst.AddEmbfile(Encoding.UTF8.GetString(b), Encoding.UTF8.GetBytes(d.FileName), d.UFileName, d.Desc);
+}
+
+dst.SaveIncremental();
