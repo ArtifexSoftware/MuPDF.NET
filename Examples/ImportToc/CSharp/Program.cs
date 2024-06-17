4a01ae0f7bae3e44619c94e0d8c8823e0095e9a2
greendreamer <chrispalman0317@gmail.com>
2024-06-17 02:34:50 -0400

    GREEN: Added import_toc csharp

---
+++ Examples/ImportToc/CSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MuPDF.NET;
+
+Document doc = new Document("input.pdf");
+
+string[] lines = File.ReadAllLines("input.csv");
+List<Toc> toc = new List<Toc>();
+
+foreach (string line in lines)
+{
+    string[] row = line.Split(';');
+    float p4 = float.Parse(row[3]);
+    Toc t = new Toc() { Level = int.Parse(row[0]), Title = row[1], Page = int.Parse(row[2]), Link = float.Parse(row[3]) };
+    toc.Add(t);
+}
+
+doc.SetToc(toc);
+doc.SaveIncremental();
