7bcc437b72c96611fa5af08687f09ef10df2a965
greendreamer <chrispalman0317@gmail.com>
2024-06-17 13:05:05 -0400

    GREEN: Added some examples

---
+++ Examples/InsertLogo/CSharp/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MuPDF.NET;
+
+Document src = new Document("logo.png");
+
+if (!src.IsPDF)
+{
+    byte[] pdfbytes = src.Convert2Pdf();
+    src.Close();
+    src = new Document("pdf", pdfbytes);
+}
+
+Rect rect = src[0].Rect;
+Console.WriteLine(rect.ToString());
+float factor = 25.0f / rect.Height;
+rect *= factor;
+
+Document doc = new Document("input.pdf");
+int xref = 0;
+for (int i = 0; i < doc.PageCount; i++)
+{
+    xref = doc[i].ShowPdfPage(rect, src, 0, overlay: false);
+}
+
+doc.Save("ouput.pdf", garbage: 4);
